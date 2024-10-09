899840dc156a092862a8240c1e6f3eca868663f6
MariosContopoulos <marioscontopoulos@hotmail.com>
2024-10-09 16:50:35 +0200

    Update Mix_List.py

--- Mix_List.py
+++ Mix_List.py
@@ -1,3 +1,6 @@
+#Creating a list with integers, floats and strings
 mix=[3,3.4,"wow"]
+#Using the comprehension list method to generate a new list with only the integers from the previous list
 integer_list=[i for i in mix if isinstance(i, int)]
+#Print the new list of integers
 print(integer_list)
