b89c2d972aebd9b19194a821749d9bcaefab49c5
MariosContopoulos <marioscontopoulos@hotmail.com>
2024-10-09 14:30:32 +0200

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,6 +1,11 @@
 def fizzbuzz(n):
-    ""
-    ""
+    """
+    The function returns
+    FizzBuzz if n is a multiple of 3 and 5
+    Buzz if n is a multiple of 5
+    Fizz if n is a multiple of 3
+    n if none of the above
+    """
     if (n%3==0 and n%5==0):
         return "FizzBuzz"
     elif n%5==0:
